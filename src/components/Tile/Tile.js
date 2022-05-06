1205943f8b0d6f66c2e1475303e5aa23542b125a
ferren ferguson <ferren.ferguson@bt.com>
2022-05-06 09:08:42 +0100

    backgroundColor

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -4,7 +4,7 @@ import "./Tile.css";
 
 const Tile = (props) => {
   const dynamicColor =
-    props.selected || props.matched ? { backgroundColor: props.color } : null;
+    props.selecteprops.matched ? { backgroundColor: props.color } : null;
   return (
     <div className="Tile" style={dynamicColor}>
       {props.selected || props.matched ? <props.svg /> } : null
